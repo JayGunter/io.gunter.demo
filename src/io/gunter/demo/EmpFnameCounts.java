4ed806e9e0a6a8be00aac71fb73c2e1cc19a2e8f
Jay Gunter <jay@gunter.io>
2017-12-22 15:07:52 -0800

    x example row class for testing whereValues

---
+++ src/io/gunter/demo/EmpFnameCounts.java
@@ -0,0 +1,19 @@
+package io.gunter.demo;
+
+import io.gunter.demo.SQL.Select;
+
+import static java.lang.System.out;
+
+import io.gunter.demo.SQL.Order;
+
+@Select(query = "select first_name as fname, count(*) as tally from employee where id > ? group by fname")
+public class EmpFnameCounts extends Row<EmpFnameCounts> {
+
+	@Order(value = 2)
+	public Integer count;
+	public String fname;
+
+	public void action() {
+		out.println("row#" + rowNum + ": fname=" + fname + ", count: " + count);
+	}
+}
